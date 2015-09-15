44a69e34390cc45dca67a2c1c1947e0810e03c92
Kelli Margaret & alexandra Brown <student@epicodus.com>
2015-09-15 15:01:43 -0700

    Pass first 3 test.

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,12 @@
+function User(userName, deposit) {
+  this.userName = userName;
+  this.deposit = deposit;
+}
 
+User.prototype.account = function() {
+  return this.userName + ", " + this.deposit;
+}
+
+User.prototype.balance = function() {
+  return this.deposit;
+}
--- spec/specs.js
+++ spec/specs.js
@@ -1,1 +1,18 @@
 
+describe('User', function() {
+  it("creates a new user with the given specifications", function() {
+     var testUser = new User("Jenny Lopez", 500);
+    expect(testUser.userName).to.equal("Jenny Lopez");
+    expect(testUser.deposit).to.equal(500);
+  });
+
+  it("adds the account information for each individual user", function() {
+    var testUser = new User("Jenny Lopez", 500);
+    expect(testUser.account()).to.equal("Jenny Lopez, 500");
+  });
+
+    it("shows a user thir account balance", function() {
+      var testUser = new User("Jenny Lopez", 500);
+      expect(testUser.balance()).to.equal(500);
+    });
+});
